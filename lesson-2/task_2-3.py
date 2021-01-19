63c1a30fc3f949b44cf408eb937c54b624ab6eff
igorrostov <ig.rostovskiy@gmail.com>
2021-01-19 14:03:00 +0300

    pythonProject1

---
+++ lesson-2/task_2-3.py
@@ -0,0 +1,17 @@
+season_list = ['Зима', 'Весна', 'Лето', 'Осень']
+season_dict = {1 : 'Зима', 2 : 'Весна', 3 : 'Лето', 4 : 'Осень'}
+month = int(input('Введите номер месяца'))
+if month == 1 or month == 12 or month == 2:
+    print(season_dict.get(1))
+    print(season_list[0])
+elif month == 3 or month == 4 or month == 5:
+    print(season_dict.get(2))
+    print(season_list[1])
+elif month == 6 or month == 7 or month == 8:
+    print(season_dict.get(3))
+    print(season_list[2])
+elif month == 9 or month == 10 or month == 11:
+    print(season_dict.get(4))
+    print(season_list[3])
+else:
+    print('Такого месяца не существует')
