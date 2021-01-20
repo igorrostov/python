87bdc55897ab3bfbc58c6f0c0fee51ce2c213d9f
igorrostov <ig.rostovskiy@gmail.com>
2021-01-20 12:24:40 +0300

    3-16 mod

--- lesson-3/task_3-16.py
+++ lesson-3/task_3-16.py
@@ -1,2 +1,10 @@
 # Создайте функцию, принимающую на вход
 # 3 числа и возвращающую наибольшее из них.
+def get_max(a, b, c):
+    result = max([a, b, c])
+    return (result)
+
+result = get_max(1, 5, 3)
+print(result)
+
+print(get_max(7, 1, 3))
